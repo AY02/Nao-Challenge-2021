db4efec4c3843bed64a0123132a40eda8c411c12
Alessio Yang <73763751+AY02@users.noreply.github.com>
2021-01-03 15:02:17 +0100

    update better listener

--- mqtt_listener.py
+++ mqtt_listener.py
@@ -1,33 +1,37 @@
 import paho.mqtt.client as mqtt
 
-clientID = 'MQTT Test Python'
+clientID = 'MQTT Listener'
 broker = 'test.mosquitto.org'
-topics = ['/root/ServoMotor', '/root/czn15e']
+root = ''
+topics = [
+    f'{root}/czn15e/Sensor1',
+    f'{root}/czn15e/Sensor2',
+    f'{root}/czn15e/Sensor3',
+    f'{root}/ServoMotor',
+]
 port = 1883
 
 def on_connect(client, userdata, flags, rc):
     print(f'Connesso con il codice: {rc}')
-
-def on_disconnect(client, userdata, rc):
-    print(f'Disconnect, reason: {rc}')
-    print(f'Disconnect, reason: {client}')
+    for topic in topics:
+        client.publish(topic, f'{clientID} e\' entrato nel topic', qos=1)
 
 def on_log(client, userdata, level, buf):
     print(f'log: {buf}')
 
 def on_message(client, userdata, msg):
-    print(f'Messaggio dal topic {msg.topic}: {msg.payload}')
+    print(f'Messaggio dal topic {msg.topic}: {msg.payload.decode()}')
 
-cli = mqtt.Client(client_id=clientID, clean_session=True, protocol=mqtt.MQTTv311, transport='tcp')
+cli = mqtt.Client(client_id=clientID)
 
 cli.on_connect = on_connect
-cli.on_disconnect = on_disconnect
 cli.on_message = on_message
 cli.on_log = on_log
 
 print('Connessione al broker...')
-cli.connect(host=broker, port=port, keepalive=60)
+cli.connect(host=broker, port=port)
 print('Connessione effettuata')
-cli.subscribe(topic=topics, qos=1)
+for topic in topics:
+    cli.subscribe(topic=topic, qos=1)
 
 cli.loop_forever()
